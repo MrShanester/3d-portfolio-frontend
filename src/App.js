73bfdb5fec8f728b05d95fb280c9e0eaa4ca32c7
Shane Nicholson <shanescottnicholson@gmail.com>
2022-11-07 11:51:24 -0800

    Sets up Three.js Scene

--- src/App.js
+++ src/App.js
@@ -31,6 +31,8 @@ function App() {
     scene.add(boxMesh);
 
     const animate = () => {
+      boxMesh.rotation.x += 0.01;
+      boxMesh.rotation.y += 0.01;
       renderer.render(scene, camera);
       window.requestAnimationFrame(animate);
     };
@@ -39,7 +41,7 @@ function App() {
 
   return (
     <div>
-      <p> Hello World</p>
+      <canvas id="myThreeJsCanvas" />
     </div>
   );
 }
